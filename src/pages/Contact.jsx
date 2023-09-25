abfbcc75b8a9c384aa6a3070c3fedc47600d4f31
Eric <esbev@hotmail.com>
2023-09-25 18:17:35 -0500

    changed image links in code

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,9 +3,9 @@ export default function Contact() {
     <div className="contact card custom-bg">
       <ul>
         <li><a href="#">esbev@hotmail.com</a></li>
-        <li><a href="https://www.linkedin.com/in/esbev/"><img className="contact-img" src="../src/assets/images/linkedin.png"></img></a></li>
-        <li><a href="https://www.github.com/esbev"><img className="contact-img" src="../src/assets/images/github.png"></img></a></li>
-        <li><a href="https://twitter.com/esbev716"><img className="contact-img" src="../src/assets/images/twitter.png"></img></a></li>
+        <li><a href="https://www.linkedin.com/in/esbev/"><img className="contact-img" src="https://ik.imagekit.io/z2zvp5n2o/linkedin.png?updatedAt=1695683630002"></img></a></li>
+        <li><a href="https://www.github.com/esbev"><img className="contact-img" src="https://ik.imagekit.io/z2zvp5n2o/github.png?updatedAt=1695683629615"></img></a></li>
+        <li><a href="https://twitter.com/esbev716"><img className="contact-img" src="https://ik.imagekit.io/z2zvp5n2o/twitter.png?updatedAt=1695683815520"></img></a></li>
       </ul>
     </div>
   );
