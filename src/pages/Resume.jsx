2cea762a0f46444ed3c87b5dd8f18cdd4f197fc6
Eric <esbev@hotmail.com>
2023-10-02 18:09:49 -0500

    worked on resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,10 +1,11 @@
 export default function Resume() {
   return (
-    <div className="container">
-      <label>Resume</l>
-      <p>
-        
-      </p>
+    <div className="card custom-bg">
+      <a className="resume" href="https://docs.google.com/document/d/1OiVA_dqIK-SN91pzfAlWto-QnPZPaU7x5FSL5OVjmPw/edit?usp=sharing">
+        Resume
+      </a>
+      <label>Proficiencies</label>
+      <p>Web Development, Web Design, JavaScript, React, HTML, jQuery, CSS, Git, Bootstrap, Materialize CSS, JSON, Node.js, Express.js, Handlebars.js, MySQL, MongoDB, MERN, Heroku, Apollo, Graphql</p>
     </div>
   );
   }
